582a30fac90579cae3ddc5074a065aaebb7a6532
SpecteriuS <91021847+SpecteriuS@users.noreply.github.com>
2021-11-30 21:02:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,63 +3,68 @@ import os
 import uuid
 import json
 from dotenv import load_dotenv
+
 load_dotenv()
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
-    
+
+
 # code
 @app.route('/', methods=['POST'])
 def index_post():
-	
-	
-	original_text = request.form['text']
-	target_language = request.form['language']
+    # Read the values from the form
+    original_text = request.form['text']
+    target_language = request.form['language']
 
-	
-	key = os.environ['KEY']
-	endpoint = os.environ['ENDPOINT']
-	location = os.environ['LOCATION']
+    # Load the values from .env
+    key = os.environ['KEY']
+    endpoint = os.environ['ENDPOINT']
+    location = os.environ['LOCATION']
 
-	
-	path = '/translate?api-version=3.0'
-	
+    # Indicate that we want to translate and the API
+    # version (3.0) and the target language
+    path = '/translate?api-version=3.0'
 
-	target_language_parameter = '&to=' + target_language
-	
+    # Add the target language parameter
+    target_language_parameter = '&to=' + target_language
 
-	constructed_url = endpoint + path + target_language_parameter
+    # Create the full URL
+    constructed_url = endpoint + path + target_language_parameter
 
+    # Set up the header information, which includes our
+    # subscription key
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
 
-	headers = {
-		'Ocp-Apim-Subscription-Key': key,
-		'Ocp-Apim-Subscription-Region': location,
-		'Content-type': 'application/json',
-		'X-ClientTraceId': str(uuid.uuid4())
-	}
+    # Create the body of the request with the text to be
+    # translated
+    body = [{'text': original_text}]
 
+    # Make the call using post
+    translator_request = requests.post(
+        constructed_url, headers=headers, json=body)
 
-	body = [{'text': original_text}]
+    # Retrieve the JSON response
+    translator_response = translator_request.json()
 
+    # Retrieve the translation
+    translated_text = translator_response[0]['translations'][0]['text']
 
-	translator_request = requests.post(
-		constructed_url, headers=headers, json=body)
-	
-
-	translator_response = translator_request.json()
-	
-
-	translated_text = translator_response[0]['translations'][0]['text']
-
-
-	return render_template(
-		'results.html',
-		translated_text=translated_text,
-		original_text=original_text,
-		target_language=target_language
-	)
-
+    # Call render template, passing the translated text,
+    # original text, and target language to the template
+    return render_template(
+        'results.html',
+        translated_text=translated_text,
+        original_text=original_text,
+        target_language=target_language
+    )
